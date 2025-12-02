33d678e4318c59be3e0d95cd70540ade3bb4ab68
Marbeck-one <Ro.martinezb@duocuc.cl>
2025-12-02 00:03:50 -0300

    actualizacion estado de servicio

--- src/reparacion/dto/update-reparacion.dto.ts
+++ src/reparacion/dto/update-reparacion.dto.ts
@@ -1,4 +1,11 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateReparacionDto } from './create-reparacion.dto';
+import { IsOptional, IsString } from 'class-validator';
+import { ApiPropertyOptional } from '@nestjs/swagger';
 
-export class UpdateReparacionDto extends PartialType(CreateReparacionDto) {}
+export class UpdateReparacionDto extends PartialType(CreateReparacionDto) {
+  @ApiPropertyOptional({ example: 'en_proceso' })
+  @IsOptional()
+  @IsString()
+  estado?: string;
+}
